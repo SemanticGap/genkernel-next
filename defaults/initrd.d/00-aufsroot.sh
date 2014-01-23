fdcd05651dea2b112ad428d1d9a7fde3ac2f9f03
Nolan Eakins <sneakin@semanticgap.com>
2014-01-23 00:25:42 -0500

    Added failure messages.

--- defaults/initrd.d/00-aufsroot.sh
+++ defaults/initrd.d/00-aufsroot.sh
@@ -38,15 +38,17 @@ aufs_root_init() {
 aufs_root_install_shutdown_hook() {
     echo <<EOF > /run/initramfs/shutdown.d/99-aufsroot.sh
 #!/bin/sh
+. /etc/initrd.d/00-common.sh
+
 for i in mnt ro rw; do
   mkdir -p /mnt/aufs/$i
-  mount -omove /newroot/mnt/root/$i /mnt/aufs/$i
+  mount -omove /newroot/mnt/root/$i /mnt/aufs/$i || bad_msg "Failed to move /newroot/mnt/root/$i"
 done
 
 umount /newroot
 
 for i in mnt ro rw; do
-  umount /mnt/aufs/$i
+  umount /mnt/aufs/$i || bad_msg "Failed to unmount /mnt/aufs/$i"
 done
 EOF
 
